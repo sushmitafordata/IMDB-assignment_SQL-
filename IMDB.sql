8c78f7aff84af840990ad40a8867ac80e0a841f3
sushmitafordata <135410984+sushmitafordata@users.noreply.github.com>
2024-04-17 00:35:46 +0200

    Update IMDB.sql

--- IMDB.sql
+++ IMDB.sql
@@ -1,14 +1,6 @@
 USE imdb;
 
-/* Now that we have imported the data sets, let’s explore some of the tables. 
- To begin with, it is beneficial to know the shape of the tables and whether any column has null values.
- Further in this segment, we will take a look at 'movies' and 'genre' tables.*/
-
-
-
 -- Segment 1:
-
-
 
 
 -- Q1. Find the total number of rows in each table of the schema?
